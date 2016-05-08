b4df3baef5bc25e76232cb58524599c9fdb6e99e
JackLap <jacklapid@yahoo.ca>
2016-05-07 22:51:15 -0400

    .

---
+++ DataModel/GameEmailEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    [Table("GameEmailEvents")]
+
+    public class GameEmailEvent : EmailEvent
+    {
+        public GameEmailEvent() : base()
+        {
+        }
+        public int DaysBeforeGame { get; set; }
+
+        public int? DaysForReoccurrence { get; set; }
+    }
+}
